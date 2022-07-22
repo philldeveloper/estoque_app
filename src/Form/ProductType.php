ad66d807d7aa814d3ed5f186d51cba770efb8f30
philip <philldeveloper@gmail.com>
2022-07-22 08:57:28 -0300

    2207

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,17 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'attr' => array(
+                    'class' => 'form-control border border-danger',
+                )
+            ])
             ->add('description')
-            ->add('created_at', DateTimeType::class)
+            ->add('created_at', DateTimeType::class, [
+                'attr' => array(
+                    'class' => 'form-select border border-danger',
+                )
+            ])
             ->add('updated_at')
         ;
     }
